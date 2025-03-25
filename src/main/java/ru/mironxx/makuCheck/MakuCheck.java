8dfd9c479b390e927926a47e7dae210ad42c4692
itsme-jay1 <mironxxtt@gmail.com>
2025-03-25 08:49:47 +0100

    Added test command

--- src/main/java/ru/mironxx/makuCheck/MakuCheck.java
+++ src/main/java/ru/mironxx/makuCheck/MakuCheck.java
@@ -1,5 +1,8 @@
 package ru.mironxx.makuCheck;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MakuCheck extends JavaPlugin {
@@ -10,6 +13,13 @@ public final class MakuCheck extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         getLogger().info(prefix + " Started!");
+
+        getCommand("check").setExecutor(new CommandExecutor() {
+            @Override
+            public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+                return false;
+            }
+        });
     }
 
     @Override
